35464cd18ad1fc93f8da895705e5096c72649805
Gustavo Pedrosa <149588552+heyguspedrosa@users.noreply.github.com>
2025-07-04 09:33:26 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const paragrafoMensagem = document.getElementById('mensagem');
+    paragrafoMensagem.textContent = "Você é capaz de conquistar tudo o que sonhar! 💪✨";
+  }
